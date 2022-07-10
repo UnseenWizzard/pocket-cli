048ad13e30c1b1328773eb0e92af5c0e26482bd9
UnseenWizzard <nico@riedmann.dev>
2022-07-10 15:04:16 +0200

    test: Fully (or overly) test http status code check

--- pkg/util/httpsuccess_test.go
+++ pkg/util/httpsuccess_test.go
@@ -1,29 +1,80 @@
 package util
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestIsHttpSuccess(t *testing.T) {
+	type statusCodeRange struct {
+		firstCode       int
+		lastCode        int
+		additionalCodes []int
+	}
 	tests := []struct {
-		name       string
-		statusCode int
-		want       bool
+		name  string
+		codes statusCodeRange
+		want  bool
 	}{
 		{
-			"http200_isSuccess",
-			200,
+			"http1xx_isNotSuccess",
+			statusCodeRange{
+				100,
+				103,
+				[]int{},
+			},
+			false,
+		},
+		{
+			"http2xx_isSuccess",
+			statusCodeRange{
+				200,
+				208,
+				[]int{226},
+			},
 			true,
 		},
 		{
-			"http403_isFailue",
-			403,
+			"http3xx_isNotSuccess",
+			statusCodeRange{
+				300,
+				308,
+				[]int{},
+			},
+			false,
+		},
+		{
+			"http4xx_isNotSuccess",
+			statusCodeRange{
+				400,
+				418,
+				[]int{421, 422, 423, 424, 425, 426, 428, 429, 431, 451},
+			},
+			false,
+		},
+		{
+			"http5xx_isNotSuccess",
+			statusCodeRange{
+				500,
+				508,
+				[]int{510, 511},
+			},
 			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := IsHttpSuccess(tt.statusCode); got != tt.want {
-				t.Errorf("IsHttpSuccess() = %v, want %v", got, tt.want)
+			for statusCode := tt.codes.firstCode; statusCode <= tt.codes.lastCode; statusCode++ {
+				testIsSuccess(t, statusCode, tt.want)
+			}
+			for _, statusCode := range tt.codes.additionalCodes {
+				testIsSuccess(t, statusCode, tt.want)
 			}
 		})
+	}
+}
+
+func testIsSuccess(t *testing.T, statusCode int, want bool) {
+	if got := IsHttpSuccess(statusCode); got != want {
+		t.Errorf("IsHttpSuccess(%v) = %v, want %v", statusCode, got, want)
 	}
 }
