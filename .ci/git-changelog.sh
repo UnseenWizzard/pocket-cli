e6e3fbed3de936a67d88c4ba064cf83089fb74ae
UnseenWizzard <nico@riedmann.dev>
2022-07-23 17:04:57 +0200

    ci: Write full changelog between versions

--- .ci/git-changelog.sh
+++ .ci/git-changelog.sh
@@ -5,6 +5,39 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-echo "# Release $1" > CHANGELOG.md
-echo "" >> CHANGELOG.md
-git log --pretty=format:"%s ([%h]($2/commits/%h))" | sort >> CHANGELOG.md
+NEW_VERSION=$1 
+
+# Get the last release tag (reverse sort by date, limited to one, return only refname, if pattern matches a hardening tag)
+LATEST_RELEASE=$(git for-each-ref --sort=-creatordate  --count=1 --format="%(refname)" refs/tags/release/*)
+
+#  Get extra short gitlog from laste release to current HEAD
+GIT_CHANGELOG=$(git log $LATEST_RELEASE..HEAD --format="- %s ([%h]($2/commits/%h))" | sort)
+
+FEATURES=""
+FIXES=""
+OTHER=""
+
+prefix=""
+while IFS= read -r line; do
+    current_prefix=$(echo "$line" | sed -E 's/-\s*(.*?):\s.*/\1/g')
+    if [ "$prefix" != "$current_prefix" ]; then
+        if [ "feat" == "$current_prefix" ]; then
+          FEATURES+="\n## 🚀 Features\n"
+        elif [ "fix" == "$current_prefix" ]; then
+          FIXES+="\n## 🐛 Fixes\n"
+        else
+          OTHER+="\n### $current_prefix\n"
+        fi
+        
+        prefix=$current_prefix
+    fi
+    if [ "feat" == "$prefix" ]; then
+        FEATURES+="$line\n"
+    elif [ "fix" == "$prefix" ]; then
+        FIXES+="$line\n"
+    else
+        OTHER+="$line\n"
+    fi
+done <<< "$GIT_CHANGELOG"
+
+printf "# Release $NEW_VERSION\n$FEATURES$FIXES\n## 🛠️ Other Changes\n$OTHER" > CHANGELOG.md
